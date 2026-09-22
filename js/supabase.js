26731bb353edc7b8fbb969062742e70e4359a978
paulo-liverpool <Paulodesousa1001@gmail.com>
2026-09-22 13:01:43 +0100

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,12 @@
+// ============================================================
+// BARÇA REAL
+// SUPABASE CLIENT
+// ============================================================
+
+const SUPABASE_URL = "https://eanimmfehvqzubgychik.supabase.co/";
+const SUPABASE_ANON_KEY = "sb_publishable_Iley6SypihdYU6w2wBVEcA_kz9Ug3DE";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
